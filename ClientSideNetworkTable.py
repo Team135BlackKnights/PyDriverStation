751fa8ada68775440d940a18872af0963374e3f1
Eaglegrant <grantman522@gmail.com>
2024-05-21 17:53:50 -0400

    timings

--- ClientSideNetworkTable.py
+++ ClientSideNetworkTable.py
@@ -13,29 +13,22 @@ NetworkTables.initialize(server="localhost") # or localhost if sim!!!
 sd = NetworkTables.getTable("SmartDashboard")
 
 data_to_robot = {
-    "test": "0.5" #comma then next value
+    "test": "0" #comma then next value
 }
 
 i = 0
 while True:
+    # Read JSON from robot
+    json_response = sd.getString("DataHandlerResponse", "default")
+    if json_response != "default":
+        data_from_robot = json.loads(json_response)
+        if "status" in data_from_robot:
+            print("Robot status:", data_from_robot["status"])
+    i +=1
+    data_to_robot["test"] = i
     # Convert dictionary to JSON string
     json_data_to_robot = json.dumps(data_to_robot)
 
     # Send JSON to robot
     sd.putString("DataHandler", json_data_to_robot)
-
-    # Read JSON from robot
-    json_response = sd.getString("DataHandlerResponse", "default")
-    if json_response != "default":
-        data_from_robot = json.loads(json_response)
-
-        # Process the data from the robot
-        print("Data from Robot:", data_from_robot)
-
-        # Example: Check for a specific key in the robot's response
-        if "status" in data_from_robot:
-            print("Robot status:", data_from_robot["status"])
-
-    sd.putNumber("dsTime", i)
-    time.sleep(1)
-    i += 1
+    time.sleep(0.105)
